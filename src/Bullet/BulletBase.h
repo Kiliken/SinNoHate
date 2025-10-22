90a6dae1f6b00b633cdfcbc2884c38a9e16b8ba5
hakesi6221 <yuanjianren000@gmail.com>
2025-10-23 00:13:47 +0900

    Add BulletBase Class

---
+++ src/Bullet/BulletBase.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Siv3D.hpp>
+
+class BulletBase{
+    protected:
+        const double m_moveSpeed = 500.0;	// 移動速度
+        Circle m_sprite{ 0, 0, 10 };		// テクスチャ
+        Vec2 m_velocity{ 0, 0 };
+
+        Color m_color;
+        Vec2 m_moveDirection;
+        Vec2 m_position;
+
+        bool m_isActive;
+
+        virtual void UpdateVelocity();
+
+    public:
+        bool IsActive();
+
+        BulletBase(Vec2 firstPosition, Vec2 firstDirection);
+
+        void Init(Vec2 firstPosition, Vec2 firstDirection);
+
+        void Update(double deltaTime);
+
+        void Draw();
+
+        void OnHit();
+};
---
+++ src/BulletBase.cpp
@@ -0,0 +1,48 @@
+#include <Siv3D.hpp>
+#include "Bullet/BulletBase.h"
+
+void BulletBase::UpdateVelocity()
+{
+    m_velocity = m_moveDirection.normalized() * m_moveSpeed;
+}
+
+bool BulletBase::IsActive()
+{
+    return m_isActive;
+}
+
+BulletBase::BulletBase(Vec2 firstPosition, Vec2 firstDirection)
+{
+    m_isActive = true;
+    m_position = firstPosition;
+    m_moveDirection = firstDirection;
+    m_color = Palette::Red;
+}
+
+void BulletBase::Init(Vec2 firstPosition, Vec2 firstDirection)
+{
+    m_isActive = true;
+    m_position = firstPosition;
+    m_moveDirection = firstDirection;
+}
+
+void BulletBase::Update(double deltaTime)
+{
+    if (!m_isActive) return;
+    UpdateVelocity();
+    m_position += m_velocity * deltaTime;
+    if (0 > m_position.x || m_position.x > Scene::Width()) m_isActive = false;
+    if (0 > m_position.y || m_position.y > Scene::Height()) m_isActive = false;
+}
+
+void BulletBase::Draw()
+{
+    if (!m_isActive) return;
+    m_sprite.setPos(m_position);
+    m_sprite.draw(m_color);
+}
+
+void BulletBase::OnHit()
+{
+    m_isActive = false;
+}
