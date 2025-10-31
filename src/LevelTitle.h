c14001e2cfbf5e9d0f6c37fe4bd47dffc1837bd1
VincentCYR7 <yuren1027@gmail.com>
2025-10-31 18:33:41 +0900

    Register level title images as resources

--- src/LevelTitle.h
+++ src/LevelTitle.h
@@ -16,13 +16,14 @@ class LevelTitle{
         
 
     private:
-        const s3d::Texture SlothText{U"Assets/LevelTitle/Sloth.png"};
-        const s3d::Texture GluttonyText{U"Assets/LevelTitle/Gluttony.png"};
-        const s3d::Texture SorrowText{U"Assets/LevelTitle/Sorrow.png"};
-        const s3d::Texture PrideText{U"Assets/LevelTitle/Pride.png"};
-        const s3d::Texture LustText{U"Assets/LevelTitle/Lust.png"};
-        const s3d::Texture EnvyText{U"Assets/LevelTitle/Envy.png"};
-        const s3d::Texture WrathText{U"Assets/LevelTitle/Wrath.png"};
+        const s3d::Texture SlothText{Resource(U"Assets/LevelTitle/Sloth.png")};
+        const s3d::Texture GluttonyText{Resource(U"Assets/LevelTitle/Gluttony.png")};
+        const s3d::Texture SorrowText{Resource(U"Assets/LevelTitle/Sorrow.png")};
+        const s3d::Texture PrideText{Resource(U"Assets/LevelTitle/Pride.png")};
+        const s3d::Texture LustText{Resource(U"Assets/LevelTitle/Lust.png")};
+        const s3d::Texture EnvyText{Resource(U"Assets/LevelTitle/Envy.png")};
+        const s3d::Texture WrathText{Resource(U"Assets/LevelTitle/Wrath.png")};
+        
         Vec2 basePos = Vec2(Scene::Width()/2, Scene::Height()/2);
         
         
